3abae3de3e82a1fde550bcbcc3f7349a48336435
unknown <petruninain@w12-426-34.main.sgu.ru>
2016-01-15 18:01:28 +0300

    modificied project on Java 15-01-2016 class Calendar

--- Task1-15-01-2016-Java/src/Friday.java
+++ Task1-15-01-2016-Java/src/Friday.java
@@ -1,7 +1,24 @@
+import sun.security.jca.GetInstance;
+import java.util.Calendar;
+import java.util.Locale;
+
 /**
- * Created by petruninain on 15.01.2016.
+ * Created by PetruninaIN on 15.01.2016.
  */
 
 public class Friday {
-    
+
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 13);
+
+        System.out.println(calendar.getTime());
+
+        System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US));
+
+    }
+
 }
