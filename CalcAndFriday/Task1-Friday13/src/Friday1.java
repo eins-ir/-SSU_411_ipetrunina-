141aad9419e189851fbdd241a4d825ff47bd711b
PetruninaIN <petruninain@gmail.com>
2016-01-17 20:09:16 +0300

    Task 1 (Friday, 13) and Task 2 (Calc) are ready

--- CalcAndFriday/Task1-Friday13/src/Friday1.java
+++ CalcAndFriday/Task1-Friday13/src/Friday1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Created by PetruninaIN on 16.01.2016.
  */
 
-// 1. Вывести название всех месяцев, в которых встречается пятница 13-е в указанном году.
+// Вывести название всех месяцев, в которых встречается пятница 13-е в указанном году.
 
 public class Friday1 {
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class Friday1 {
         Calendar calendar = Calendar.getInstance();
 
         //определяем переменную с воскресеньем
-        String FindDay = "Sun";
+        String FindDay = "Fri";
         //определяем счетчик
         int kol = 0;
 
@@ -28,11 +28,10 @@ public class Friday1 {
         //проверка на валидность года
         if (year < 2021 && year > 1899) {
             //присвоение года и числа
-            calendar.set(Calendar.DAY_OF_MONTH, 1);
+            calendar.set(Calendar.DAY_OF_MONTH, 13);
             calendar.set(Calendar.YEAR, year);
 
             //проход по всем месяцам.
-            //поиск, если воскресенье попадает на 1 число.
             for (int i = 0; i < 11; i++) {
                 //присвоение месяца
                 calendar.set(Calendar.MONTH, i);
@@ -42,10 +41,8 @@ public class Friday1 {
                     System.out.println(calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US));
                 }
             }
-
-            System.out.println("In " + year + " Friday, 13 was " + kol);
-
+            System.out.println("В " + year + " пятниц 13 было " + kol);
         }
-        else System.out.println("Choose other year! 1900 <= year <= 2020");
+        else System.out.println("Год должен быть в числовом формате от 1900 до 2020");
     }
 }
--- CalcAndFriday/Task1-Friday13/src/Friday2.java
+++ CalcAndFriday/Task1-Friday13/src/Friday2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Created by PetruninaIN on 16.01.2016.
  */
 
-//2. Вывести года, где пятница 13-е попадается в указанном месяце.
+// Вывести года, где пятница 13-е попадается в указанном месяце.
 // Вывести количество совпадений.
 // Проверку проводить до сегодняшнего дня.
 // Сделать проверку входных данных на валидность.
@@ -16,51 +16,86 @@ public class Friday2 {
         Calendar calendar = Calendar.getInstance();
 
         //определяем переменную с воскресеньем
-        String FindDay = "Sun";
+        String FindDay = "Fri";
         //определяем счетчик
         int kol = 0;
 
 
         //консольный ввод месяца
         Scanner scaner = new Scanner(System.in);
-        System.out.println("Enter month:");
+        System.out.println("Введите месяц:");
         int month = Integer.parseInt(scaner.nextLine());
 
         //консольный ввод года
-        System.out.println("Enter year:");
+        System.out.println("Введите год, с которого нужно начинать подсчет пятниц 13:");
         int year = Integer.parseInt(scaner.nextLine());
+        int yearenter = year;
 
 
         //сохранение данных о сегодняшнем дне
-        //String NowDayOfWeek = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
-        //String NowMonth = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US);
+        String NowDayOfWeek = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+        int NowMonth = MonthCase(calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US));
         int NowYear = 2016;
-        //проверка на то, что месяц введенный меньше или равен месяцу текущему. Чтобы не перепрыгнуть текущую дату в подсчетах пятниц.
-        //  if (month > NowMonth) NowYear--;
 
 
-        //проверка на валидность года
-        if (year < NowYear && year > 1900 && month >= 0 && month < 12) {
+        //проверка на то, что месяц введенный меньше или равен месяцу текущему, если введенный год совпадает с текущим.
+        // Чтобы не перепрыгнуть текущую дату в подсчетах пятниц.
+        if (month > NowMonth && NowYear == year) NowYear--;
+
+
+        //проверка на правильность введенных вх.данных
+        if (year <= NowYear && year > 1899 && month >= 0 && month < 12) {
             //присвоение года, месяца и числа
-            calendar.set(Calendar.DAY_OF_MONTH, 1);
+            calendar.set(Calendar.DAY_OF_MONTH, 13);
             calendar.set(Calendar.MONTH, month);
             calendar.set(Calendar.YEAR, year);
-
 
             //проход по всем годам.
             //поиск, если воскресенье попадает на 1 число.
             while (year <= NowYear) {
-
                 if (calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US) == FindDay) {
                     kol += 1;
                     System.out.println(year);
                 }
-
                 year++;
                 calendar.set(Calendar.YEAR, year);
             }
-
-            System.out.println("number of coincidences is equal " + kol);
+            System.out.println("С " + yearenter + " года количество пятниц 13 равно " + kol);
         }
+        else System.out.println("Введите правильные значения! Месяц в числовом формате от 0 до 11. Год в числовом формате от 1900 до 2016");
+    }
+
+    //перевод месяца в числовой вид
+    private static int MonthCase(String monthString){
+        int month;
+        switch (monthString) {
+            case "Jan": month = 0;
+                break;
+            case "Feb": month = 1;
+                break;
+            case "Mar": month = 2;
+                break;
+            case "Apr": month = 3;
+                break;
+            case "May": month = 4;
+                break;
+            case "Jun": month = 5;
+                break;
+            case "Jul": month = 6;
+                break;
+            case "Aug": month = 7;
+                break;
+            case "Sep": month = 8;
+                break;
+            case "Oct": month = 9;
+                break;
+            case "Nov": month = 10;
+                break;
+            case "Dec": month = 11;
+                break;
+            default: month = 15;
+                break;
+        }
+        return month;
     }
 }
