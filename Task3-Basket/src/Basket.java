4507393caa69e6483684da12aeafee32503e4d69
PetruninaIN <petruninain@gmail.com>
2016-01-22 13:48:11 +0300

    Task 3. Interface of basket

--- Task3-Basket/src/Basket.java
+++ Task3-Basket/src/Basket.java
@@ -3,11 +3,11 @@ import java.util.List;
 /**
  * Created by PetruninaIN on 22.01.2016.
  */
-    public interface Basket {
-        void addProduct(String product, int quantity);
-        void removeProduct(String product);
-        void updateProductQuantity(String product, int quantity);
-        void clear();
-        List<String> getProducts();
-        int getProductQuantity(String product);
-    }
+public interface Basket {
+    void addProduct(String product, int quantity);
+    void removeProduct(String product);
+    void updateProductQuantity(String product, int quantity);
+    void clear();
+    List<String> getProducts();
+    int getProductQuantity(String product);
+}
--- Task3-Basket/src/BasketMain.java
+++ Task3-Basket/src/BasketMain.java
@@ -3,38 +3,35 @@ import java.util.List;
 /**
  * Created by PetruninaIN on 22.01.2016.
  */
-public class BasketMain {
-    public static void main(String[] args) {
-        Basket baskets = new Basket() {
-            @Override
-            public void addProduct(String product, int quantity) {
+public class BasketMain implements Basket{
 
-            }
+        @Override
+        public void addProduct(String product, int quantity) {
+        }
 
-            @Override
-            public void removeProduct(String product) {
+        @Override
+        public void removeProduct(String product) {
 
-            }
+        }
 
-            @Override
-            public void updateProductQuantity(String product, int quantity) {
+        @Override
+        public void updateProductQuantity(String product, int quantity) {
 
-            }
+        }
 
-            @Override
-            public void clear() {
+        @Override
+        public void clear() {
 
-            }
+        }
 
-            @Override
-            public List<String> getProducts() {
-                return null;
-            }
+        @Override
+        public List<String> getProducts() {
+            return null;
+        }
 
-            @Override
-            public int getProductQuantity(String product) {
-                return 0;
-            }
-        };
+        @Override
+        public int getProductQuantity(String product) {
+            return 0;
+        }
     }
-}
+
