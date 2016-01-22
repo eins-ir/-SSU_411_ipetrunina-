45938a7ccd84fce89f459035ede884bcec3614a7
unknown <petruninain@w12-416-31.main.sgu.ru>
2016-01-22 17:37:52 +0300

    Change BasketMain

--- Task3-Basket/src/BasketMain.java
+++ Task3-Basket/src/BasketMain.java
@@ -8,50 +8,60 @@ import java.util.*;
 
 //???????? ??? map
 
-public class BasketMain implements Basket{
+public class BasketMain{
+        public static void main(String[] args) {
 
         Map<String, Integer> hm = new HashMap<String, Integer>();
 
-        @Override
-        public void addProduct(String product, int quantity) {
-                hm.put(product,quantity);
-        }
+        Basket basket = new Basket() {
+                @Override
+                public void addProduct(String product, int quantity) {
+                        hm.put(product,quantity);
+                }
 
-        @Override
-        public void removeProduct(String product) {
-                hm.remove(product);
-        }
+                @Override
+                public void removeProduct(String product) {
+                        hm.remove(product);
+                }
 
-        @Override
-        public void updateProductQuantity(String product, int quantity) {
-                int value = hm.get(product);
-                hm.put(product, value + quantity);
-        }
+                @Override
+                public void updateProductQuantity(String product, int quantity) {
+                        int value = hm.get(product);
+                        hm.put(product, value + quantity);
+                }
 
-        @Override
-        public void clear() {
+                @Override
+                public void clear() {
+                        hm.clear();
+                }
 
-        }
+                @Override
+                public List<String> getProducts() {
+                        Set<Map.Entry<String, Integer>> set = hm.entrySet();
+                        for (Map.Entry<String, Integer> me : set) {
+                                System.out.print(me.getKey());
+                        }
+                        return null;
+                }
 
-        @Override
-        public List<String> getProducts() {
-            return null;
-        }
+                @Override
+                public int getProductQuantity(String product) {
+                        Set<Map.Entry<String, Integer>> set = hm.entrySet();
+                        for (Map.Entry<String, Integer> me : set) {
+                                System.out.print(me.getKey()+ ": ");
+                                System.out.println(me.getValue());
+                        }
+                        return 0;
+                }
+        };
 
-        @Override
-        public int getProductQuantity(String product) {
-            return 0;
-        }
-
-        public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter product:");
-                String product = scanner.nextLine();
-                System.out.println("Enter quantity:");
-                int quantity = Integer.parseInt(scanner.nextLine());
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter product:");
+        String product = scanner.nextLine();
+        System.out.println("Enter quantity:");
+        int quantity = Integer.parseInt(scanner.nextLine());
+        //basket.addProduct(product,quantity);
+       // basket.getProducts();
 
         }
     }
