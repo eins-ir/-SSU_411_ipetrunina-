f8b67fa3465206ea707be08c6ea12ce72e87bbd1
unknown <petruninain@w12-416-31.main.sgu.ru>
2016-01-22 17:10:25 +0300

    Change BasketMain

--- Task3-Basket/src/BasketMain.java
+++ Task3-Basket/src/BasketMain.java
@@ -1,25 +1,31 @@
-import java.util.List;
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
+
+import java.util.*;
 
 /**
  * Created by PetruninaIN on 22.01.2016.
  */
 
 //???????? ??? map
-        
+
 public class BasketMain implements Basket{
+
+        Map<String, Integer> hm = new HashMap<String, Integer>();
 
         @Override
         public void addProduct(String product, int quantity) {
+                hm.put(product,quantity);
         }
 
         @Override
         public void removeProduct(String product) {
-
+                hm.remove(product);
         }
 
         @Override
         public void updateProductQuantity(String product, int quantity) {
-
+                int value = hm.get(product);
+                hm.put(product, value + quantity);
         }
 
         @Override
@@ -35,6 +41,18 @@ public class BasketMain implements Basket{
         @Override
         public int getProductQuantity(String product) {
             return 0;
+        }
+
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter product:");
+                String product = scanner.nextLine();
+                System.out.println("Enter quantity:");
+                int quantity = Integer.parseInt(scanner.nextLine());
+
+
+
+
         }
     }
 
